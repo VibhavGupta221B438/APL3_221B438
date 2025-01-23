bc1c7ad7fa364ce5c6dbe8a5af5db3b660b57c43
221B438 <221B438@juet.co.in>
2025-01-23 15:02:09 +0530

    Lab Ex 1

---
+++ LE1/PROB3/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+		Mother m=new Mother();
+		Mother m1=new Child();
+		m.show();
+		Child ch=new Child();
+		ch.show();
+		m1.show();
+	}
+}
